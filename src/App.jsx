dae45c01444ef747ce96263829b6cdb78e5b8a87
Omindu Hirushka <102536488+OminduHirushka@users.noreply.github.com>
2025-04-02 09:50:16 +0530

    dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <div>
-        <h1>Blog Application</h1>
-      </div>
-    </>
+    <Routes path="/*">
+      <Route path="/*" element={<Home />} />
+    </Routes>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { UploadOutlined, UserOutlined } from "@ant-design/icons";
+import { Layout, Menu, theme } from "antd";
+import { Link } from "react-router-dom";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const Home = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  return (
+    <Layout>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+        style={{
+          width: "50px",
+        }}
+      >
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          style={{
+            marginTop: "20px",
+            paddingTop: "20px",
+            fontSize: "18px",
+          }}
+        >
+          <Menu.Item key={"1"} icon={<UserOutlined />}>
+            <Link to={"/"}>Posts</Link>
+          </Menu.Item>
+          <Menu.Item key={"2"} icon={<UploadOutlined />}>
+            <Link to={"/"}>Add Posts</Link>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }} />
+        <Content style={{ margin: "24px 16px 0" }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 600,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          ></div>
+        </Content>
+
+        <Footer style={{ textAlign: "center" }}>
+          Omindu Hirushka
+          <br />©{new Date().getFullYear()}
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+export default Home;
